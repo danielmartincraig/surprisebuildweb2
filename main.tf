cb6e148a97cab90ba6a7cc3b5b0831e4173c5dc8
danielmartincraig <danielmartincraig@gmail.com>
2025-06-07 08:51:55 -0500

    changed lb target group type to instance

--- main.tf
+++ main.tf
@@ -174,7 +174,7 @@ resource "aws_lb_target_group" "example" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.example.id
-  target_type = "ip"
+  target_type = "instance" 
 
   health_check {
     path                = "/"
